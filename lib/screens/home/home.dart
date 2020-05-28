7498bde22ec4ac73b02311186052de7277d5f5a4
edwardlhwang <edwardhwang@berkeley.edu>
2020-05-28 16:07:48 -0700

    added bottom sheet

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -11,6 +11,18 @@ class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
+
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text('bottom sheet')
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -26,6 +38,12 @@ class Home extends StatelessWidget {
                 },
                 icon: Icon(Icons.person),
                 label: Text('Logout')),
+            FlatButton.icon(
+                onPressed: () {
+                  _showSettingsPanel();
+                },
+                icon: Icon(Icons.settings),
+                label: Text("Settings"))
           ]
         ),
         body: BrewList(),
