3ffb4fdbbb5566f97f4820171cc88d438386950f
edwardlhwang <edwardhwang@berkeley.edu>
2020-05-27 22:51:30 -0700

    implement basic firestore stream

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,24 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebasetutorial/screens/services/auth.dart';
+import 'package:firebasetutorial/screens/services/database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'brew_list.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text('Home Page'),
-        backgroundColor: Colors.blue[300],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Logout')),
-        ]
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.blue[50],
+        appBar: AppBar(
+          title: Text('Home Page'),
+          backgroundColor: Colors.blue[300],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text('Logout')),
+          ]
+        ),
+        body: BrewList(),
       ),
     );
   }
--- lib/screens/services/database.dart
+++ lib/screens/services/database.dart
@@ -15,4 +15,9 @@ class DatabaseService {
     });
   }
 
+  // get brews stream
+  Stream<QuerySnapshot> get brews {
+    return brewCollection.snapshots();
+  }
+
 }
