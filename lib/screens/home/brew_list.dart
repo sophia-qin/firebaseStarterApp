d70c613d355bc4987c9fef4290fbc9f021400a44
edwardlhwang <edwardhwang@berkeley.edu>
2020-05-28 14:49:57 -0700

    display firestore data on home page

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
+import 'brew_tile.dart';
+
 class BrewList extends StatefulWidget {
   @override
   _BrewListState createState() => _BrewListState();
@@ -12,11 +14,12 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    brews.forEach((brew) {
-      print(brew.name);
-      print(brew.sugars);
-    });
-    return Container();
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
 
 
   }
