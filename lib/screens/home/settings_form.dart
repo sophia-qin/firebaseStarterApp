63e5f3876f6cf258a2cc9e5cf0b7efabd6774279
edwardlhwang <edwardhwang@berkeley.edu>
2020-05-29 18:31:08 -0700

    added user ability to update database

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,6 +1,9 @@
+import 'package:firebasetutorial/models/user.dart';
+import 'package:firebasetutorial/screens/services/database.dart';
 import 'package:firebasetutorial/shared/constants.dart';
+import 'package:firebasetutorial/shared/loading.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -8,7 +11,6 @@ class SettingsForm extends StatefulWidget {
 }
 
 class _SettingsFormState extends State<SettingsForm> {
-
   final _formKey = GlobalKey<FormState>();
   final List<String> sugars = ['0', '1', '2', '3'];
 
@@ -18,39 +20,59 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text('Update brew settings'),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration.copyWith(hintText: 'Update Name'),
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(height: 20.0),
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            value: _currentSugars ?? '0', //not completeley necessary anymore
-            items: sugars.map((sugar) {
-              return DropdownMenuItem(
-                value: sugar,
-                child: Text('$sugar sugars'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(()=> _currentSugars = val),
-          ),
-          RaisedButton(
-            color: Colors.blue,
-            child: Text('Update'),
-            onPressed: () async {
-              print(_currentName);
-              print(_currentSugars);
-            },
-          )
-        ],
-      ),
-    );
+    // passed down from the wrapper
+    final user = Provider.of<User>(context);
+
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData userData = snapshot.data;
+            return Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Text('Update brew settings'),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    initialValue: userData.name,
+                    decoration:
+                        textInputDecoration.copyWith(hintText: 'Update Name'),
+                    validator: (val) =>
+                        val.isEmpty ? 'Please enter a name' : null,
+                    onChanged: (val) => setState(() => _currentName = val),
+                  ),
+                  SizedBox(height: 20.0),
+                  DropdownButtonFormField(
+                    decoration: textInputDecoration,
+                    value: _currentSugars ?? userData.sugars,
+                    //not completeley necessary anymore
+                    items: sugars.map((sugar) {
+                      return DropdownMenuItem(
+                        value: sugar,
+                        child: Text('$sugar sugars'),
+                      );
+                    }).toList(),
+                    onChanged: (val) => setState(() => _currentSugars = val),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    child: Text('Update'),
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        await DatabaseService(uid: user.uid).updateUserData(
+                          _currentSugars ?? userData.sugars, _currentName ?? userData.name
+                        );
+                        Navigator.pop(context);
+                      }
+                    },
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Loading();
+          }
+        });
   }
 }
